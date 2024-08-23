3cbbbc2c310b1c7f84e9404fbdf24bb9e170240d
GCharles85 <gcharles345@gmail.com>
2024-08-23 18:05:06 -0400

    add placeholders for work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Page() {
                     </a>
                   </h3>
                   <div className="text-sm tabular-nums text-gray-500">
-                    {work.start} - {work.end}
+                    {`${work.start} - ${work.end}`} w
                   </div>
                 </div>
                 <h4 className="font-mono text-sm leading-none">
