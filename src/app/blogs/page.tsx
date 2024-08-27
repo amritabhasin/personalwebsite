bfdb6ac532a068e9b049e64c15245cdafce3f77e
GCharles85 <gcharles345@gmail.com>
2024-08-26 23:16:15 -0400

    fixed apos

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,7 +4,7 @@ const BlogsPage = () => {
     return (
         <div>
             <h1>Blogs</h1>
-            <p>Welcome to the blogs page. Here you'll find all the latest posts.</p>
+            <p>Welcome to the blogs page. Here you&apos;ll find all the latest posts.</p>
             {/* Add your blog content here */}
         </div>
     );
