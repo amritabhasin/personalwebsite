34d05e802514d09b1493a018566b9100be3405dc
GCharles85 <gcharles345@gmail.com>
2024-08-26 20:52:01 -0400

    add style to Global icon tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,16 +22,17 @@ export default function Page() {
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
               {RESUME_DATA.about}
             </p>
-            
+            <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
               <a
                 className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
                 // href={RESUME_DATA.locationLink}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Globe className="size-3" />
+                <Globe className="size-3" style={{ color: 'inherit' }}/>
                 {RESUME_DATA.location}
               </a>
+            </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
               {RESUME_DATA.contact.email && (
                 <Button
